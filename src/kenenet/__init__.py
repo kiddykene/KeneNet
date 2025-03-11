6fc2965cf66e1691e9d80e7bf5c34b5831e4abc6
kiddykene <kittyking360@gmail.com>
2025-03-11 12:37:56 -0600

    caca

--- src/kenenet/__init__.py
+++ src/kenenet/__init__.py
@@ -1,8 +1,13 @@
-import inspect, sys, zhmiscellany, keyboard, mss
+import inspect, sys, zhmiscellany, keyboard, mss, time, sys
 
-global get_pos_cooldown
+global timings
+timings = {}
 def adding(a, b):
     return a + b
+
+def quick_print(message):
+    sys.stdout.write(f"\033[94m{message}\033[0m\n")
+
 
 def get_pos(key='f10', kill=False):
     def _get_pos(key):
@@ -15,11 +20,21 @@ def get_pos(key='f10', kill=False):
                 rgb = screenshot.pixel(0, 0)
             color = f"\033[38;2;{rgb[0]};{rgb[1]};{rgb[2]}m"
             reset = "\033[0m"
-            print(f"Coordinates: ({x}, {y}), RGB: {rgb} {color}████████{reset}")
+            quick_print(f"Coordinates: ({x}, {y}), RGB: {rgb} {color}████████{reset}")
             if kill:
-                print('killing process')
+                quick_print('killing process')
                 zhmiscellany.misc.die()
     zhmiscellany.processing.start_daemon(target=_get_pos, args=(key,))
+
+def time_it(clock=1):
+    if clock in timings:
+        elapsed = timings[clock] - time.time()
+        print()
+    else:
+        timings[clock] = time.time()
+    frame = inspect.currentframe().f_back
+    filename = frame.f_code.co_filename
+    lineno = frame.f_lineno
 
     
 class k:
