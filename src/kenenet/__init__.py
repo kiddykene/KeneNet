ad77e694527eebdf3a6d8cb496f75c49dd495e02
kiddykene <kittyking360@gmail.com>
2025-03-11 11:59:32 -0600

    caca

--- src/kenenet/__init__.py
+++ src/kenenet/__init__.py
@@ -1,21 +1,28 @@
-import inspect, sys
+import inspect, sys, zhmiscellany, keyboard
+from PIL import ImageGrab
 
 def adding(a, b):
     return a + b
 
-def subtracting(a, b):
-    return a - b
-
+def get_pos(key='f10', kill=False):
+    def _get_pos(key):
+        keyboard.wait(key)
+        image = ImageGrab.grab()
+        x, y = zhmiscellany.misc.get_mouse_xy()
+        rgb = image.getpixel((x, y))
+        print(f"Coordinates: ({x}, {y}), RGB: {rgb}")
+        if kill:
+            print('killing process')
+            zhmiscellany.misc.die()
+    zhmiscellany.processing.start_daemon(target=_get_pos, args=(key,))
+    
 class k:
     pass
 
-# Automatically attach all global functions to class k,
-# filtering out any that start with an underscore (like __init__)
 current_module = sys.modules[__name__]
 for name, func in inspect.getmembers(current_module, inspect.isfunction):
     if not name.startswith('_'):
         setattr(k, name, func)
 
-# Optional: update __main__ if needed
 if '__main__' in sys.modules:
     sys.modules['__main__'].__dict__['k'] = k
